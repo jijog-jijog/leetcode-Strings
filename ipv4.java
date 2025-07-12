4501db4ebfd8e8175c4d9d56ba58dc2f037fed12
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-12 20:30:23 +0530

    w

--- ipv4.java
+++ ipv4.java
@@ -1,7 +1,20 @@
+
+
 public class ipv4 {
     public static void main(String[] args) {
         String address = "255.100.50.0";
 
-        System.out.println(address);
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < address.length(); i++) {
+            int val = address.charAt(i);
+            if (val == '.') {
+                  builder.append("[.]");
+            }else{
+                 builder.append(val);
+            }
+        }
+        System.out.println(builder);
+
     }
 }
