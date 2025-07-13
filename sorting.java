06bac5a4e13ba02b0ff3176954149864e02d9233
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-13 21:08:55 +0530

    s

--- sorting.java
+++ sorting.java
@@ -1,8 +1,20 @@
+
+
 public class sorting {
     public static void main(String[] args) {
         
         String s = s = "is2 sentence4 This1 a3";
 
-           
+        String[] split = s.split(" ");
+        String[] res = new String[split.length];
+
+        for(String a : split){
+            int len = a.length();
+            int pos = a.charAt(len - 1) - '0';
+
+            res[pos-1] = a.substring(0,len-1);
+        }
+        // System.out.println(String.join(" ", res));
+        System.out.println(new String(res));
     }
 }
