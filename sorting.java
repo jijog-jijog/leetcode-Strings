2a2b922a10c3d85164ef590379cd3b1d0e7d5234
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-13 20:24:22 +0530

    3

--- sorting.java
+++ sorting.java
@@ -3,15 +3,6 @@ public class sorting {
         
         String s = s = "is2 sentence4 This1 a3";
 
-         String[] words = s.split(" ");                     // Step 1: Split words
-        String[] result = new String[words.length];        // Step 2: Prepare result array
-
-        for (String word : words) {
-            int len = word.length();
-            int pos = word.charAt(len - 1) - '0';          // Step 3: Get position
-            result[pos - 1] = word.substring(0, len - 1);  // Step 4: Remove digit and place
-        }
-
-        return String.join(" ", result);     
+           
     }
 }
