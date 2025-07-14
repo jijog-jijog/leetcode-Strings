8254ee0824e6a08a22cbf04b53338c6942ca3d4b
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 14:28:16 +0530

    p

--- decryptstring.java
+++ decryptstring.java
@@ -2,14 +2,34 @@ public class decryptstring {
     public static void main(String[] args) {
         
         String s = "10#11#12";
+        StringBuilder str = new StringBuilder();
+         int i = s.length()-1;
 
-         for(int i=s.length()-1; i>=0; i--){
+         while (i >= 0) {
             char ch = s.charAt(i);
+            if (ch == '#') {
+                int change = Integer.parseInt(s.substring(i-2,i));
 
-            if (ch >= 'a' && ch<= 'i') {
-                
+                char  value = (char)('a' + change-1);
+
+                str.append(value);
+
+                i = i - 3;
+            }else{
+                char ch2 = s.charAt(i);
+
+                int num = (ch2) - '0';
+
+                char val2 = (char)(num-1 + 'a');
+
+                str.append(val2);
+                i = i - 1;
+
             }
-            System.out.println(ch);
          }
+
+         System.out.println(str.reverse().toString());
+      
+         
     }
 }
