9cc9bd718e3d7c2e580f231b784a2e5aa97c9e99
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 19:10:51 +0530

    d

---
+++ firstoccurence.java
@@ -0,0 +1,10 @@
+public class firstoccurence {
+    public static void main(String[] args) {
+        
+        String hays = "sadbutsad";
+        String needle = "sad";
+
+        int con = needle.contains(hays);
+
+    }
+}
