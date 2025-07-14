e28b08ae5cc7653241d9ac905ad07c572f861940
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 10:41:36 +0530

    d

---
+++ decryptstring.java
@@ -0,0 +1,15 @@
+public class decryptstring {
+    public static void main(String[] args) {
+        
+        String s = "10#11#12";
+
+         for(int i=s.length()-1; i>=0; i--){
+            char ch = s.charAt(i);
+
+            if (ch) {
+                
+            }
+            System.out.println(ch);
+         }
+    }
+}
