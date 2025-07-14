b421c3f1d288efbe48932ea8a5e4dd8ca0946b02
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 22:30:28 +0530

    e

--- palin2.java
+++ palin2.java
@@ -2,6 +2,24 @@ public class palin2 {
     public static void main(String[] args) {
         
         String s = "abca";
+        StringBuilder build = new StringBuilder(s);
         
+        int i =0;
+        int j = s.length()-1;
+
+        boolean ans = true;
+    //     while (i < j) {
+    //         if (s.charAt(i) == s.charAt(j))  {
+    //             i++;
+    //             j--;
+    //         }else if (s.charAt(i) != s.charAt(j)) {
+    //             // s.charAt(j).remove;
+    //         }
+    //         else {
+    //             ans = false;
+    //             break;
+    //     }
+    // }
+        System.out.println(build.toString());
     }
 }
