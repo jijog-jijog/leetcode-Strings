3222187a98029169f6223c1e90ff446c8d986040
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 15:44:42 +0530

    s

--- revstring.java
+++ revstring.java
@@ -1,10 +1,15 @@
+
+import java.util.Arrays;
+
 public class revstring {
     public static void main(String[] args) {
         String s = "Let's take LeetCode contest";
 
         StringBuilder str = new StringBuilder();
 
-        
+        String[] split = s.split(" ");
+
+        System.out.println(Arrays.toString(split));
 
     }
 }
