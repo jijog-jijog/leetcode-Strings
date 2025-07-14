b61c9c0e79f5331098fb97deaf32845af91ad6e4
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 15:41:29 +0530

    s

---
+++ revstring.java
@@ -0,0 +1,10 @@
+public class revstring {
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+
+        StringBuilder str = new StringBuilder();
+
+        
+
+    }
+}
