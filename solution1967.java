030575120ae86c4e4fc79d750f77b3ce07e30dc7
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 14:31:06 +0530

    e

--- solution1967.java
+++ solution1967.java
@@ -1,9 +1,11 @@
 public class solution1967 {
     public static void main(String[] args) {
         String[] patterns = {"a","abc","bc","d"};
+        String word = "abc";
         
         for(String a : patterns){
-            
+
+            System.out.println(a);
         }
     }
 }
