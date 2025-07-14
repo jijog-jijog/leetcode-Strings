2ea3f983f5a167e399a3b85b81647f84b15c149f
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 21:57:10 +0530

    Implement palindrome check logic in palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,27 @@
+
+
+public class palindrome {
+    public static void main(String[] args) {
+        
+        String str = "A man, a plan, a cnal: Panama";
+
+        String rep = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int i = 0;
+        int j = rep.length()-1;
+        boolean ans = true;
+
+        while (i < j) {
+            if (rep.charAt(i) == rep.charAt(j)) {
+                i++;
+                j--;
+            }else{
+                ans = false;
+                break;
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
