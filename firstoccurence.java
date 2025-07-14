9b38305b66506802a66f85448c135ca0e4c1901b
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 19:49:02 +0530

    Refactor firstoccurence: update input strings and remove unnecessary variable

--- firstoccurence.java
+++ firstoccurence.java
@@ -1,10 +1,22 @@
+
+
 public class firstoccurence {
     public static void main(String[] args) {
         
-        String hays = "sadbutsad";
-        String needle = "sad";
+        String hays = "jijoleetcode";
+        String needle = "leetco";
 
-        int con = needle.contains(hays);
+        int val = -1;
+
+        String[] split = hays.split(" ");
+
+        for(int i =0; i <split.length; i++){
+            if (split[i].contains(needle)) {
+                val = hays.indexOf(needle);
+            }
+        }
+
+        System.out.println(val);
 
     }
 }
