5dedfde87facee851e93d980e80bcf4294e9f28e
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 14:45:11 +0530

    n

--- solution1967.java
+++ solution1967.java
@@ -1,22 +1,19 @@
 public class solution1967 {
     public static void main(String[] args) {
-        String[] patterns = {"a","abc","bc","d"};
-        String word = "abc";
+        String[] patterns = {"a","b","c"};
+        String word = "aaaaabbbbb";
         
         int count = 0;
         for(String a : patterns){
-            for(int i=0; i<patterns.length; i++){
-                char ch = word.charAt(i);
-                char ah = a.charAt(i);
-                
-                if (ch == ah) {
-                    count = count + 1;
-                    break;
-                }
-            }
+             if (word.contains(a)) {
+                count++;
+             }
+            
             // System.out.println(a);
             
         }
         System.out.println(count);
-    }
+  }
+  
 }
+
