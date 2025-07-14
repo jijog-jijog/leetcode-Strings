d0496f7150550bf8da7998f941a85d8f1425cb24
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 22:47:56 +0530

    Refactor palindrome check logic in palin2.java to improve clarity and efficiency

--- palin2.java
+++ palin2.java
@@ -1,26 +1,49 @@
 public class palin2 {
     public static void main(String[] args) {
-        
-        String s = "abca";
-        StringBuilder build = new StringBuilder(s);
-        
-        int i =0;
-        int j = s.length()-1;
+     String s = "abda";
 
-        boolean ans = true;
+        int i = 0;
+        int j = s.length() - 1;
+        boolean valid = true;
+
         while (i < j) {
-            if (build.charAt(i) == build.charAt(j))  {
+            if (s.charAt(i) == s.charAt(j)) {
                 i++;
                 j--;
-            }if (build.charAt(i) != build.charAt(j)) {
-                 build.deleteCharAt(j);
-                 j--;
+            } else {
+                // Try skipping left character (i + 1)
+                boolean skipLeft = true;
+                int x = i + 1, y = j;
+                while (x < y) {
+                    if (s.charAt(x) != s.charAt(y)) {
+                        skipLeft = false;
+                        break;
+                    }
+                    x++;
+                    y--;
+                }
+
+                // Try skipping right character (j - 1)
+                boolean skipRight = true;
+                x = i;
+                y = j - 1;
+                while (x < y) {
+                    if (s.charAt(x) != s.charAt(y)) {
+                        skipRight = false;
+                        break;
+                    }
+                    x++;
+                    y--;
+                }
+
+                if (!skipLeft && !skipRight) {
+                    valid = false;
+                }
+
+                break; // We've already used our one allowed skip
             }
-            else {
-                ans = false;
-                break;
         }
-    }
-        System.out.println(ans);
+
+        System.out.println(valid);
     }
 }
