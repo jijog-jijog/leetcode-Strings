8ff58a5e9276a26bc827002e7891da0e0a578c10
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-14 20:26:50 +0530

    Implement initial logic for character matching in longpressed925

--- longpressed925.java
+++ longpressed925.java
@@ -3,5 +3,17 @@ public class longpressed925 {
         
         String name = "alex";
         String typed = "aaleex";
+
+        boolean ans = false;
+        for(int i =0; i<name.length(); i++){
+            char ch = name.charAt(i);
+            for(int j=0; j<typed.length();j++){
+                char ty = typed.charAt(j);
+                if (ch == ty) {
+                    ans = true;
+                }
+            }
+        }
+        
     }
 }
