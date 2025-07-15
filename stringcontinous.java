27482ff2dd63a737bb6a23abce6fe0f3d39056b1
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-15 16:27:36 +0530

    e

--- stringcontinous.java
+++ stringcontinous.java
@@ -1,5 +1,15 @@
 public class stringcontinous {
     public static void main(String[] args) {
-        
+        String s = "1001";
+
+        boolean ans = false;
+        for(int i =0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if (ch == s.charAt(i + 1)) {
+                ans = true;
+            }
+        }
+        System.out.println(ans);
     }
 }
