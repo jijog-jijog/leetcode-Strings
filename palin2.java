06bd7370d03c5104fe1d3a659914e1e96dfef100
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-15 12:42:20 +0530

    Refactor palindrome check logic in palin2.java for improved clarity and efficiency

--- palin2.java
+++ palin2.java
@@ -1,51 +1,47 @@
 public class palin2 {
     public static void main(String[] args) {
-     String s = "abcddddfgha";
+     String s = "abca";
+ int i = 0;
+        int j = s.length() - 1;
+        boolean valid = true;
 
-     int i =0;
-     int j = s.length()-1;
-
-     boolean ans = false;
-     while (i < j) {
-        if (s.charAt(i) == s.charAt(j)) {
-            i++;
-            j--;
-            ans = true;
-            break;
-        }else if (s.charAt(i) != s.charAt(j)) {
-            boolean leftskip = true;
-            int x = i+1;
-            int y = j;
-            while (x <= j) {
-                if (s.charAt(x) != s.charAt(y)) {
-                    leftskip = false;
-                    break;
-                }else{
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                // Check skipping left char
+                boolean skipLeft = true;
+                int x = i + 1,
+                 y = j;
+                while (x <= y) {
+                    if (s.charAt(x) != s.charAt(y)) {
+                        skipLeft = false;
+                        break;
+                    }
                     x++;
                     y--;
                 }
-            }
 
-            boolean rightskip = true;
-            int xx = i;
-            int yy = j-1;
-
-            while (xx <= yy) {
-                if (s.charAt(xx) == s.charAt(yy)) {
-                    rightskip = false;
-                    break;
-                }else{
-                    xx++;
-                    yy--;
+                // // Check skipping right char
+                boolean skipRight = true;
+                x = i;
+                y = j - 1;
+                while (x <= y) {
+                    if (s.charAt(x) != s.charAt(y)) {
+                        skipRight = false;
+                        break;
+                    }
+                    x++;
+                    y--;
                 }
-            }
-            if (leftskip || rightskip == true) {
-                ans = true;
-                break;
+
+                valid = skipLeft || skipRight;
+                // valid = skipLeft;
+                break;  // One deletion allowed only
             }
         }
+        System.out.println(valid);
        
-     }
-    System.out.println(ans);
     }
 }
