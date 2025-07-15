23a1f909d979fad1a1125ec31a1d7013735181f1
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-15 16:02:14 +0530

    4

--- maximumrepet.java
+++ maximumrepet.java
@@ -2,11 +2,17 @@ public class maximumrepet {
     public static void main(String[] args) {
         String sequence = "ababc",
                  word = "ab";
+        StringBuilder build = new StringBuilder(word);
 
         int count = 0;
-        if (sequence.contains(word)) {
+        while (count < sequence.length()) {
+             if (build.toString().contains(word)) {
             count++;
+            build.append(word);
+            
         }
+        }
+       
         System.out.println(count);
     }
 }
