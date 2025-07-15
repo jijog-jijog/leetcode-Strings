b8fced6e13cf70884e3e951eed209d1f876582d2
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-15 11:54:25 +0530

    g

--- palin2.java
+++ palin2.java
@@ -1,49 +1,50 @@
 public class palin2 {
     public static void main(String[] args) {
-     String s = "abda";
+     String s = "abcccdda";
 
-        int i = 0;
-        int j = s.length() - 1;
-        boolean valid = true;
+     int i =0;
+     int j = s.length()-1;
 
-        while (i < j) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
-            } else {
-                // Try skipping left character (i + 1)
-                boolean skipLeft = true;
-                int x = i + 1, y = j;
-                while (x < y) {
-                    if (s.charAt(x) != s.charAt(y)) {
-                        skipLeft = false;
-                        break;
-                    }
+     boolean ans = false;
+     while (i < j) {
+        if (s.charAt(i) == s.charAt(j)) {
+            i++;
+            j--;
+            ans = false;
+        }else if (s.charAt(i) != s.charAt(j)) {
+            boolean leftskip = true;
+            int x = i+1;
+            int y = j;
+            while (x <= j) {
+                if (s.charAt(x) != s.charAt(y)) {
+                    leftskip = false;
+                    break;
+                }else{
                     x++;
                     y--;
                 }
+            }
 
-                // Try skipping right character (j - 1)
-                boolean skipRight = true;
-                x = i;
-                y = j - 1;
-                while (x < y) {
-                    if (s.charAt(x) != s.charAt(y)) {
-                        skipRight = false;
-                        break;
-                    }
-                    x++;
-                    y--;
+            boolean rightskip = true;
+            int xx = i;
+            int yy = j-1;
+
+            while (xx <= yy) {
+                if (s.charAt(xx) == s.charAt(yy)) {
+                    rightskip = false;
+                    break;
+                }else{
+                    xx++;
+                    yy--;
                 }
-
-                if (!skipLeft && !skipRight) {
-                    valid = false;
-                }
-
-                break; // We've already used our one allowed skip
+            }
+            if (leftskip || rightskip == true) {
+                ans = true;
+                break;
             }
         }
-
-        System.out.println(valid);
+       
+     }
+    System.out.println(ans);
     }
 }
