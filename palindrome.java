33afdb25d2f502db362ab3a3013ca4f5d4f1178a
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-17 21:59:07 +0530

    g

--- palindrome.java
+++ palindrome.java
@@ -4,13 +4,14 @@ public class palindrome {
     public static void main(String[] args) {
         
         String str = "A man, a plan, a cnal: Panama";
+        
 
         String rep = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
         int i = 0;
 
         int j = rep.length()-1;
-        
+
 
         boolean ans = true;
 
